fa176d1a0676b7d8c9ae35cc267cb6a910d04e69
Joao Carlos Poloniato <jcpoloniato@gmail.com>
2018-01-25 19:08:04 -0200

    incluso a comparacao do evalor com o pvalor do ks em diferentes cenarios

--- calculo-evalor.R
+++ calculo-evalor.R
@@ -1,15 +1,37 @@
-setwd('/home/joao_polo/Dropbox/Disciplinas/Mestrado/Dissertação/fbst-nparam')
+setwd('/home/joao_polo/Dropbox/Mestrado/Dissertacao/fbst-nparam')
 source('funcoes-uteis.R') 
 
-#evalor modificado
+#carregando pacotes de devem ser usados
+library(nortest)
+library(tidyverse)
 
-ev.mod<-function(dados,phi,r,peso,simu=1000){
-  n<-length(dados)
-  tau<-peso^(1:r)
-  x<-phi_barra(dados,phi,r)
-  aux<-evalue(x,n,tau)
+#funcao que calcula o evalor não modificado
+#dados - dados
+#phi -base ortonormal
+#r - tamanho da sequencia da base ortonormal
+#peso - peso para cada elemento da sequencia
+ev_nmod <- function(dados, phi, r, peso){
+  n <- length(dados)
+  tau <- peso^(1:r)
+  x <- phi_barra(dados, phi, r)
+  aux <- testeFBST(x, n, tau)
+
+  1 - pchisq(aux, df=r)
+}
+
+#funcao que calcula o evalor modificado
+#dados - dados
+#phi -base ortonormal
+#r - tamanho da sequencia da base ortonormal
+#peso - peso para cada elemento da sequencia
+#simu - numero de observacoes que devem ser geradas da distribuicao de comparacao
+ev_mod <- function(dados, phi, r, peso, simu=1000){
+  n <- length(dados)
+  tau <- peso^(1:r)
+  x <- phi_barra(dados, phi, r)
+  aux <- testeFBST(x, n, tau)
   
-  q<-rep(NA, simu)
+  q <- rep(NA, simu)
   for (i in 1:simu){
     q[i] <- sum(((n*rnorm(r,sd=1/sqrt(n)))/sqrt(n+tau))^2)
   }
@@ -17,75 +39,102 @@ ev.mod<-function(dados,phi,r,peso,simu=1000){
   1 - mean(q < aux)
 }
 
-ev.nmod<-function(dados,phi,r,peso){
-  n<-length(dados)
-  tau<-peso^(1:r)
-  x<-phi_barra(dados,phi,r)
-  aux<-evalue(x,n,tau)
-
-  1 - pchisq(aux, df=r)
-}
-
-#comparacao entre evalor modificado e nao modificado 
-
-#H_0 falsa
-evm<-rep(NA,1000)
-evnm<-rep(NA,1000)
-for (i in 1:1000){
-  dados<-rnorm(10000,mean=5,sd=1)
-  evm[i]<-ev.mod(dados,phi,50,5)
-  evnm[i]<-ev.nmod(dados,phi,50,5)
-}
-par(mfrow=c(1,2))
-hist(evm)
-hist(evnm)
+##################################################################################
+##### ESSE BLOCO COMPARA A DISTRIBUICAO DO EVALOR MODIFICADO E NAO MODIFICADO ####
+##################################################################################
 
 #H_0 verdadeira
-evm1<-rep(NA,1000)
-evnm1<-rep(NA,1000)
+evm0 <- rep(NA,1000)
+evnm0 <- rep(NA,1000)
 for (i in 1:1000){
-  dados<-runif(10000)
-  evm1[i]<-ev.mod(dados,phi,50,5)
-  evnm1[i]<-ev.nmod(dados,phi,50,5)
+  dados <- runif(10000)
+  evm0[i] <- ev_mod(dados, phi, r = 50, peso = 5)
+  evnm0[i] <- ev_nmod(dados, phi, r = 50, peso = 5)
+}
+par(mfrow=c(1,2))
+hist(evm0)
+hist(evnm0)
+
+#H_0 falsa
+evm1 <- rep(NA,1000)
+evnm1 <- rep(NA,1000)
+for (i in 1:1000){
+  dados <- rnorm(10000, mean=5, sd=1)
+  evm1[i] <- ev_mod(dados, phi, 50, 5)
+  evnm1[i] <- ev_nmod(dados, phi, 50, 5)
 }
 hist(evm1)
 hist(evnm1)
 
-####################################
-# gerar dados e aplicar uma F nos dados e 
-# verificar se os dados modificado tem distribuicao uniforme
+#################################################################################
 
-# funcao que vai calcular o e valor nos dados transformados
+#gerar dados e aplicar uma F nos dados e 
+#verificar se os dados modificado tem distribuicao uniforme
 
-ev.mod.pf<-function(dados, phi, r, peso, pf){
-  dados.inv <- inversa(dados, pf)
-  ev.mod(dados.inv, phi, r, peso)
+#funcao que vai calcular o evalor nos dados transformados (padronizados)
+#dados - dados
+#phi -base ortonormal
+#r - tamanho da sequencia da base ortonormal
+#peso - peso para cada elemento da sequencia
+#simu - numero de observacoes que devem ser geradas da distribuicao de comparacao
+#pf - funcao F que deve ser usada para padronizar os dados
+ev_mod_pf<-function(dados, phi, r = 50, peso = 5, simu=1000, pf){
+  ev <- dados %>%  pf() %>% ev_mod(phi, r, peso, simu = simu)
+  ev
 }
 
-library(nortest)
-# funcao que vai calcular o pvalor do ks nos dados transformados
-
-pvalor.ks<-function(dados, pf){
-  dados.inv <- inversa(dados, pf)
-  Test <- ks.test(dados.inv, punif)
-  round(Test$'p.value', 4)
+#funcao que vai calcular o pvalor do ks nos dados transformados
+#dados - dados
+#pf - funcao F que deve ser usada para padronizar os dados
+pvalor_ks <- function(dados, pf){
+  test <- dados %>% pf() %>% ks.test(punif)
+  test$'p.value'
 }
 
-# essa funcao compara o ks com o evalor, por isso dados deve ser uma matriz de dados
-evalor.vs.ks<-function(dados, phi, r, peso, pf){
-  
-  ev<-apply(dados,2,ev.mod.pf,phi=phi,r= r,peso= peso,pf= pf)
-  pv<-apply(dados,2,pvalor.ks,pf=pf)
-  cbind(ev,pv)
+#essa funcao retorna uma matriz com a primeira coluna de evalore e a outra de pvalor ks
+evalor_vs_ks <- function(matriz_dados, phi, r = 50, peso = 5, pf){
+  ev <- apply(matriz_dados, 2, ev_mod_pf, phi = phi, r = r, peso = peso, pf = pf)
+  pv <- apply(matriz_dados, 2, pvalor_ks, pf = pf)
+  cbind(ev, pv)
 }
 
-dados <- matrix(rnorm(10000,0,1),ncol=100)
-evalor.vs.ks(dados,phi,50,5,pnorm)
-evalor.vs.ks(dados,phi,50,5,pexp)
+#funcao para ver a proporcao de rejeicao fixado um alpha
+poder_ev_vs_ks <- function(matriz_dados, phi, r = 50, peso = 5, pf, alpha = 0.05){
+  poder <- colMeans(evalor_vs_ks(dados, phi, 50, 5 , pf) < alpha)
+  poder
+}
 
-dados <- matrix(rnorm(10000,5,1),ncol=100)
-evalor.vs.ks(dados,phi,50,5,pnorm)
+#mesma funcao usando pipe
+poder_ev_vs_ks1 <- function(matriz_dados, phi, r = 50, peso = 5, pf, alpha = 0.05){
+  poder <- evalor_vs_ks(dados, phi, 50, 5 , pf) %>% 
+          as.tibble() %>% 
+          summarise(erro_ev = mean(ev < alpha), erro_ks = mean(pv < alpha))
+  poder
+}
 
-dados<- matrix(rgamma(10000,5,1),ncol=100)
-evalor.vs.ks(dados,phi,50,5,pnorm)
-evalor.vs.ks(dados,phi,50,5,pgamma)
+#########################################################
+############ COMPARACOES DE DIFERENTES CENARIOS #########
+#########################################################
+
+#H0 verdadeira
+set.seed(87)
+
+dados <- matrix(rnorm(100000,0,1),ncol=100)
+pf<-function(x) pnorm (x , mean=0, sd=1)
+poder_ev_vs_ks(dados, phi, pf= pf)
+poder_ev_vs_ks1(dados, phi, pf= pf) # as vezes da um numero diferente 
+
+dados <- matrix(rexp(100000, rate = 2),ncol=100)
+pf<-function(x) pexp (x , rate = 2)
+poder_ev_vs_ks(dados, phi, pf= pf)
+
+
+#H0 falsa
+dados <- matrix(rnorm(100000,0,1),ncol=100)
+pf<-function(x) pnorm (x , mean=5, sd=1)
+poder_ev_vs_ks(dados, phi, pf= pf)
+
+dados <- matrix(rexp(100000, rate = 2),ncol=100)
+pf<-function(x) pexp (x , rate = 1)
+poder_ev_vs_ks(dados, phi, pf= pf)
+ 
--- funcoes-uteis.R
+++ funcoes-uteis.R
@@ -2,8 +2,14 @@
 
 #funcao de x para calcular a i-esimo funcao da base ortonormal (phi) de cossenos
 phi <- function(i, x){
-  invisible( sqrt(2) * cos(i * pi * x) )
+  sqrt(2) * cos(i * pi * x)
 }
+
+#funcao de x para calcular a i-esimo funcao da base ortonornal de  senos e cossenos
+#PROBLEMA NAO ESTA ESTIMANDO BEM A DENSIDADE
+# phi2 <- function(i, x){
+#   return(ifelse(i%%2,sqrt(2)*sin((i+1)*pi*x),sqrt(2)*cos((i*pi*x))))
+# } 
 
 #funcao que transforma os dados usando a sequencia ortonormal de funcoes
 #dados - dados a serem transformados
@@ -14,11 +20,11 @@ phi_barra <- function(dados, phi, r){
   for (i in 1:r){
     phi_barra[i] <- mean(phi(i, dados))
   } 
-  invisible(phi_barra)
+  phi_barra
 }
 
 #funcao que recebe como parametros os dados ja tranformados (phi barra)
 #e calcula a estatistica de teste do FBST
 testeFBST <- function(x, n, tau){
-  invisible( sum(( n* x /sqrt(n + tau) ) ^2) )
+ sum(( n * x /sqrt(n + tau) ) ^2)
 }
