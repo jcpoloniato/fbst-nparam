6da9dc69a24d3da59f89b576c5d38d9ed0ea48c5
jcpoloniato <33102263+jcpoloniato@users.noreply.github.com>
2018-10-23 22:43:16 -0300

    versao final do teste para 2 populacoes

--- funcaopoder_fbst2pop.R
+++ funcaopoder_fbst2pop.R
@@ -1,7 +1,5 @@
 source('funcoes uteis.R') 
 
-#### ESSE DEU CERTO \o/ 
-
 #######################################################
 ### METODO 2 - FAZENDO BOOTSTRAP PARA DEFINIR O CORTE #
 #######################################################
@@ -55,7 +53,7 @@ calculo_ev_mod_boot<-function(dados1, dados2, phi, r=50, peso = function(x) x^2,
   1 - mean(q < aux)
 }
 
-simulacao<-1000
+simulacao<-500
 
 ##########
 # NORMAL #
@@ -396,6 +394,153 @@ mist_norm <- function (n, medias, variancias, probs){
   
   vetor
 }
+
+#1 pop com dist normal(0,1) e outra de mistura de 2 normais
+
+variancias <- c(1,1)
+medias<-matrix(c(-seq(0,3,by=0.1),seq(0,3,by=0.1)),ncol=2)
+
+#n = 50
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(50,0,1)
+    data2<-mist_norm(50, medias=medias[b,], variancias=c(1,1), probs=c(0.5,0.5))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm2n0_tam50-50')
+
+#n = 100
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(100,0,1)
+    data2<-mist_norm(100, medias=medias[b,], variancias=c(1,1), probs=c(0.5,0.5))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm2n0_tam100-100')
+
+#n = 500
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(500,0,1)
+    data2<-mist_norm(500, medias=medias[b,], variancias=c(1,1), probs=c(0.5,0.5))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm2n0_tam500-500')
+
+
+## 1 pop de N(0,1) e outra de mistura de 3 normais
+
+medias<-matrix(c(-seq(0,3,by=0.1),seq(0,3,by=0.1),rep(0,length(seq(0,3,by=0.1)))),ncol=3)
+
+#n = 50
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(50,0,1)
+    data2<-mist_norm(50, medias=medias[b,], variancias=c(1,1,1), probs=c(1/3,1/3,1/3))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm3n0_tam50-50')
+
+#n = 100
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(100,0,1)
+    data2<-mist_norm(100, medias=medias[b,], variancias=c(1,1,1), probs=c(1/3,1/3,1/3))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm3n0_tam100-100')
+
+#n = 500
+poder <- matrix(NA, length(medias),2)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=2,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-rnorm(500,0,1)
+    data2<-mist_norm(500, medias=medias[b,], variancias=c(1,1,1), probs=c(1/3,1/3,1/3))
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi)
+    resul.teste[a,2]<-ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=c(medias[,2],medias[,2]),
+                poder=c(poder[,1], poder[,2]) ,
+                teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2popmistnorm3n0_tam500-500')
+
+
+###Duas pop de mistura de normais
+mist_norm <- function (n, medias, variancias, probs){
+  k <- length(probs)
+  vetor<-rep(NA,n)
+  
+  aux<-rmultinom(n, size = 1, prob = probs)
+  delta<-rep(NA,n)
+  j<-1
+  while (j < (k+1)){
+    delta[aux[j,]==1]<-j
+    j <- j+1
+  }
+  
+  j<-1
+  while(j < (k+1)){
+    vetor[delta==j]<-rnorm(sum(delta==j),medias[j],variancias[j])
+    j<-j+1
+  }
+  
+  vetor
+}
 #mistura de 2 normais
 variancias <- c(1,1)
 medias<-matrix(c(-seq(0,3,by=0.1),seq(0,3,by=0.1)),ncol=2)
@@ -457,7 +602,7 @@ dat<-data.frame(medias=c(medias[,2],medias[,2]),
                 teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
 saveRDS(dat, file='2popmistnorm2_tam500-500')
 
-#mistura de 3 normais
+#2 pop de mistura de 3 normais
 medias<-matrix(c(-seq(0,3,by=0.1),seq(0,3,by=0.1),rep(0,length(seq(0,3,by=0.1)))),ncol=3)
 
 #n = 50
@@ -640,3 +785,206 @@ dat<-data.frame(medias=c(medias, medias),
                 poder=c(poder[,1], poder[,2]), 
                 teste=c(rep('ev', length(medias)), rep('pv.ks', length(medias))))
 saveRDS(dat, file='2popbetameio_tam500-500')
+
+
+###########################
+# TRIGONOMETRICA  vs UNIF #
+###########################
+
+densidade<-function(x,k){
+  abs(sin(pi*k*x)*pi/2)
+}
+gerar<-function(n,k){
+  gerado<-rep(NA,n)
+  i<-1
+  while(is.na(gerado[n])){
+    aux1<- runif(1)
+    aux2<- runif(1,0,pi/2)
+    
+    if(densidade(aux1,k) < aux2){
+      gerado[i]<-aux1
+      i<-i+1
+    }
+  }
+  gerado
+}
+medias<-seq(1,30,by=1)
+
+###2 pop trigo 50 
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(50)
+    data2<-gerar(50, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2poptrigounif_tam50-50')
+
+###2 pop trigo 100 
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-gerar(100, 5)
+    data2<-gerar(100, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2poptrigo_tam100-100')
+
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(100)
+    data2<-gerar(100, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2poptrigounif_tam100-100')
+
+###2 pop trigo 500 
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(500)
+    data2<-gerar(500, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2poptrigounif_tam500-500')
+
+############################
+# UNIFORME DE HAAR vs UNIF##
+############################
+
+unif.haar<- function(x, k) floor(k*x) %% 2
+runif.haar<- function(n,k){
+  gerado<- rep(NA,n)
+  i<-1
+  while(is.na(gerado[n])){
+    aux<-runif(1)
+    if(unif.haar(aux,k)){
+      gerado[i]<-aux
+      i <- i+1
+    }
+  }
+  gerado
+}
+medias<-2^seq(1,30,by=1)
+
+##2 pop haar 50
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(50)
+    data2<-runif.haar(50, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2pophaarunif_tam50-50')
+
+##2 pop haar 100
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(100)
+    data2<-runif.haar(100, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2pophaarunif_tam100-100')
+
+##2 pop haar 500
+poder <- matrix(NA, length(medias),5)  
+for(b in 1:length(medias)){
+  resul.teste<-matrix(NA,ncol=5,nrow = simulacao)
+  for(a in 1:simulacao){
+    data1<-runif(500)
+    data2<-runif.haar(500, medias[b])
+    resul.teste[a,1] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^2)
+    resul.teste[a,2] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) x^5)
+    resul.teste[a,3] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 2^x)
+    resul.teste[a,4] <- calculo_ev_mod_boot(data1,data2,phi=phi,peso=function(x) 5^x)
+    resul.teste[a,5] <- ks.test(data1,data2)$'p.value'
+  }
+  poder[b,]<-colMeans(resul.teste<0.05)
+  print(b)  
+}
+
+dat<-data.frame(medias=rep(1:30,5),
+                poder=c(poder[,1], poder[,2], poder[,3], poder[,4], poder[,5]) ,
+                teste=c(rep('x2', length(medias)), rep('x5', length(medias)), rep('2x', length(medias)), 
+                        rep('5x', length(medias)), rep('pv.ks', length(medias))))
+saveRDS(dat, file='2pophaarunif_tam500-500')
